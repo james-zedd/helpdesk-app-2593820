4448b0d9b4208b7b7a41049c5518071977c3042b
james-zedd <james_zedd@hotmail.com>
2022-03-12 17:23:11 -0500

    Done lessons 122 - 126

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,12 +1,101 @@
-const registerUser = (req, res) => {
-    res.send('Register route');
-};
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
-const loginUser = (req, res) => {
-    res.send('Login user');
+// @desc   Register a new user
+// @route  /api/users
+// @auth?  false
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // validation
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('User data not accepted. Please include all fields.');
+        return;
+    }
+
+    // find if user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create User
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+    res.send('Register route');
+});
+
+// @desc   login a user
+// @route  /api/users/login
+// @auth?  false
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    // check for user and if password is match
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid credentials.');
+    }
+});
+
+// @desc   get current user
+// @route  /api/users/me
+// @auth?  true
+const getme = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+    };
+    res.status(200).json({
+        status: 200,
+        data: user,
+    });
+});
+
+// generate token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '20m',
+    });
 };
 
 module.exports = {
     registerUser,
     loginUser,
+    getme,
 };
--- api/server.js
+++ api/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// database connect
+connectDB();
+
+// middleware - parse json and url encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes -- userRoutes
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the helpdesk app' });
 });
-
 app.use('/api/users', require('./routes/userRoutes'));
+
+// error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} ...`));
