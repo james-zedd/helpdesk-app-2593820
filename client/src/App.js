00326583ec4aa978100b41c95246d8b3e12b06f3
james-zedd <james_zedd@hotmail.com>
2022-03-18 08:47:14 -0400

    Done lessons 142 to 144

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NewTicket from './pages/NewTicket';
+import Tickets from './pages/Tickets';
+import Ticket from './pages/Ticket';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -20,6 +22,18 @@ function App() {
                         <Route path='/register' element={<Register />} />
                         <Route path='/new-ticket' element={<PrivateRoute />}>
                             <Route path='/new-ticket' element={<NewTicket />} />
+                        </Route>
+                        <Route path='/tickets' element={<PrivateRoute />}>
+                            <Route path='/tickets' element={<Tickets />} />
+                        </Route>
+                        <Route
+                            path='/ticket/:ticketId'
+                            element={<PrivateRoute />}
+                        >
+                            <Route
+                                path='/ticket/:ticketId'
+                                element={<Ticket />}
+                            />
                         </Route>
                     </Routes>
                 </div>
