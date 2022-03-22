93a06946b58f25bddf83115c171cf3c35e8e3964
james-zedd <james_zedd@hotmail.com>
2022-03-22 09:04:42 -0400

    Prepare for deploy

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -14,16 +15,23 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// routes -- main
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the helpdesk app' });
-});
-
 // routes -- userRoutes
 app.use('/api/users', require('./routes/userRoutes'));
 
 // routes -- ticketRoutes
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// map production frontend to build folder in react
+if (process.env.NODE_ENV === production) {
+    app.use(express.static(path.join(_dirname, '../client/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(__dirname, '../', 'client', 'build', 'index.html')
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the helpdesk app' });
+    });
+}
 
 // error handler
 app.use(errorHandler);
